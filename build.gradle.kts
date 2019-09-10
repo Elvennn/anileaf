949844d07d1ed97b7c25180a5941f6f8a86caa87
adrien baudhuin <a.baudhuin13@gmail.com>
2019-09-10 20:51:05 +0200

    feat: settings

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,14 +10,13 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
-    jcenter()
 }
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
     testCompile("junit", "junit", "4.12")
     compile("com.jayway.jsonpath", "json-path" ,"2.4.0")
-    runtime("com.fasterxml.jackson.core:jackson-databind:2.4.5")
+    compile("com.fasterxml.jackson.module", "jackson-module-kotlin", "2.9.+")
     compile("org.slf4j", "slf4j-simple", "1.7.28")
     compile("org.simpleframework", "simple-xml", "2.7.1")
     compile("com.dgtlrepublic", "anitomyJ", "0.0.7")
